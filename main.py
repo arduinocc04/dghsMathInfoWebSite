200b635a6adcfe96b1979ddc18ab9ce5f620fb8a
arduinocc04 <arduinocc04@gmail.com>
2021-03-11 10:13:00 +0900

    preety completed

--- main.py
+++ main.py
@@ -10,13 +10,27 @@ def home():
 def team():
     return render_template('team.html')
 
+@app.route('/constructing')
+def constructing():
+    return render_template('constructing.html')
+
 @app.route('/recollection')
 def recollection():
-    return render_template('recollection.html')
+    #return render_template('recollection.html')
+    return redirect(url_for('constructing'))
 
 @app.route('/history')
 def history():
-    return render_template('history.html')
+    #return render_template('history.html')
+    return redirect(url_for('constructing'))
+
+@app.route('/howToEnroll')
+def howToEnroll():
+    return render_template('howToEnroll.html')
+
+@app.route('/promotion')
+def promotion():
+    return render_template('promotion.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080)
